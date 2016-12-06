640fb9f9ee94584ee1305547f82530d9ded6f272
Marcin Nowak <nowak@swissquant.com>
2016-12-06 11:24:47 +0100

    serenity put in place and operational

--- src/test/java/net/sonyx/p4/tests/RunCukeTest.java
+++ src/test/java/net/sonyx/p4/tests/RunCukeTest.java
@@ -1,15 +1,14 @@
 package net.sonyx.p4.tests;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 /**
  * Created by Marcin on 05-Dec-16.
  */
 
-
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "net.sonyx.p4.tests"
--- src/test/resources/features/Position.feature
+++ src/test/resources/features/Position.feature
@@ -1,4 +1,7 @@
 Feature: Position movements
+  if I have a position
+  and I decide to move it
+  then it should be moved
 
 Scenario: fresh to move
   Given we have fresh position
